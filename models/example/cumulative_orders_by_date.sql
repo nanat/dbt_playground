1b3398a8589795aba8840fdf5bd4a4dbd87e2687
nanat <simona.valenti.0@gmail.com>
2023-09-13 16:47:46 +0100

    add tags and target conditions

--- models/example/cumulative_orders_by_date.sql
+++ models/example/cumulative_orders_by_date.sql
@@ -1,5 +1,13 @@
 {{ config(materialized='table') }}
 
+with sample_orders as (
+    select *
+    from {{ source('snowflake_sample', 'orders') }}
+)
+
 select o.o_orderdate, sum(o.o_totalprice) as cumulative_sales
-from snowflake_sample_data.tpch_sf1.orders o
+from sample_orders o
+{% if target.name == 'dev' %}
+    where year(o_orderdate) = 1996
+{% endif %}
 group by o.o_orderdate
--- models/example/snowflake_customer_purchases.sql
+++ models/example/snowflake_customer_purchases.sql
@@ -7,9 +7,18 @@
 --group by c.c_custkey, c.c_name, c.c_nationkey
 --having sum(o.o_totalprice) > 0
 
+with sample_customer as (
+    select *
+    from {{ source('snowflake_sample', 'customer') }}
+),
+sample_orders as (
+    select *
+    from {{ source('snowflake_sample', 'orders') }}
+)
+
 select c.c_custkey, c.c_name, c.c_nationkey, sum(o.o_totalprice) as total_order_price
-from {{ source("snowflake_sample", "customer") }} c
-left join {{ source("snowflake_sample", "orders")}} o
+from sample_customer c
+left join sample_orders o
 on c.c_custkey = o.o_custkey
 {{ group_by(3) }} --macro
 having sum(o.o_totalprice) > 0
